3168e2180c5420df4171970b2d7153b6eeb07760
Mohamed Hassan Elwakeel <mo7mdhassan7@gmail.com>
2025-12-30 19:53:17 +0200

    Add app icons and update global styles for ListBook

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
+import Image from 'next/image';
+import logo from '@/public/Logo.png';
+
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+    <div className="flex min-h-screen flex-col items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main>
-        <h1 className="text-4xl font-bold">Hello World this ListBook Project</h1>
+        <h1 className="text-primary text-4xl font-bold">Hello World this ListBook Project</h1>
+        <Image src={logo} alt="ListBook Logo" className="size-10" />
       </main>
     </div>
   );
